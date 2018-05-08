36267fbbe9762375fb3ccfe29a40a1b497840f07
[adriangalende] <[adriangalende@gmail.com]>
2018-05-08 19:48:19 +0200

    test_obte_telefon

--- src/main/java/org/formacio/mvc/ControllerContactos.java
+++ src/main/java/org/formacio/mvc/ControllerContactos.java
@@ -1,9 +1,12 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,5 +19,12 @@ public class ControllerContactos {
     @ResponseBody
     public int obtenerNumeroContactos(){
         return agendaService.nombreContactes();
+    }
+
+    @RequestMapping(path="/telefon", method=RequestMethod.GET)
+    @ResponseBody
+    public String obtenerContactoId(String id){
+        //recupera(id) devuelve un objeto persona, que tiene el método getTelefon, que devuelve un String
+        return agendaService.recupera(id).getTelefon();
     }
 }
